09a59d903b5f4dd611f53f2e54659c20ffae1435
KingJeremyNg <jeremy97ng@gmail.com>
2019-05-17 22:43:37 -0400

    Added user schema

---
+++ src/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userModel = new mongoose.Schema({
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    card_number: { type: String },
+    orders: [
+        { order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' } },
+    ],
+    current_order: {
+        order_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+    },
+});
+
+module.exports = mongoose.model('User', userModel);
