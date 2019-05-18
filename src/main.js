e430da1f5be7a6fe5833c96f1969289b5010f57f
squishy123 <christian.gnaw@gmail.com>
2019-05-17 22:31:36 -0400

    Added express init

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//get env vars
+require('dotenv').config();
+
+//create server
+let server = express();
+
+server.use(bodyParser.json());
+
+server.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+(async function() {
+    server.listen(3000, function() {
+        console.log(`${server.name} listening at http://localhost:3000`);
+    });
+})();
