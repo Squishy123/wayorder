54c0b10f6c204a976ab64c21d00b641dc6533ca2
squishy123 <christian.gnaw@gmail.com>
2019-05-17 23:40:42 -0400

    Added helper plugins and started backend write

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+//plugins
+import connectDB from './modules/connectDB';
+import RouteLoader from './modules/routeLoader';
+
 //get env vars
 require('dotenv').config();
 
@@ -17,6 +21,13 @@ server.use(
 );
 
 (async function() {
+    //load all routes
+    let routeLoader = new RouteLoader(server, {dir: path.join(__dirname, './routes'), verbose: true, strict: false});
+    await routeLoader.loadDir();
+
+    //connect to DB
+    await connectDB();
+
     server.listen(3000, function() {
         console.log(`${server.name} listening at http://localhost:3000`);
     });
---
+++ src/routes/test/test.js
@@ -0,0 +1,8 @@
+module.exports = {
+    path: '/',
+    method: 'GET',
+    handler: (req, res) => {
+        res.contentType = 'json';
+        res.send({message: "Hello World!"});
+    }
+}
