4f08dc3035615812d0115bb9e380606291cbd916
KingJeremyNg <jeremy97ng@gmail.com>
2019-05-17 23:54:57 -0400

    Added order schema

---
+++ src/models/order.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const orderModel = new mongoose.Schema({
+    status: { type: String, enum: ['pending', 'cancelled', 'completed'], default: 'pending', required: true },
+    total: { type: Number },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    meta: { type: Object },
+});
+
+module.exports = mongoose.model('Order', orderModel);
