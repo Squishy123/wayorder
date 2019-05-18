c340f795be92a01e02c6a44fb2e636e0e3061787
squishy123 <christian.gnaw@gmail.com>
2019-05-18 05:09:27 -0400

    added product schema

---
+++ src/models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const productModel = new mongoose.Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    description: String,
+    //in millis
+    prep_time: Number
+});
+
+module.exports = mongoose.model('Product', productModel);
