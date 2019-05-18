7b383b687dd489658f0c57edde5ab617266f783a
squishy123 <christian.gnaw@gmail.com>
2019-05-18 05:57:44 -0400

    Added working login route

--- src/models/user.js
+++ src/models/user.js
@@ -16,7 +16,7 @@ const userModel = new mongoose.Schema({
     email_confirm: { type: String },
 });
 
-userModel.methods.addHashedPassword = async function(password) {
+userModel.methods.addHashedPassword = async function (password) {
     let hashed = await new Promise((resolve, reject) => {
         bcrypt.genSalt(10, (err, salt) => {
             if (err) reject(err);
@@ -31,7 +31,7 @@ userModel.methods.addHashedPassword = async function(password) {
     return hashed;
 };
 
-userModel.methods.createEmailConfirmationToken = async function() {
+userModel.methods.createEmailConfirmationToken = async function () {
     /** let confirmationToken = await jwt.sign(
         { user_id: this._id },
         process.env.SECRET,
@@ -51,7 +51,7 @@ userModel.methods.createEmailConfirmationToken = async function() {
     return this.email_confirm;
 };
 
-userModel.statics.verifyEmailConfirmationToken = async function(
+userModel.statics.verifyEmailConfirmationToken = async function (
     confirmationToken
 ) {
     //let decoded = await jwt.verify(confirmationToken, process.env.SECRET);
@@ -81,5 +81,45 @@ userModel.statics.verifyEmailConfirmationToken = async function(
         message: 'Invalid confirmation token.',
     };
 };
+
+userModel.statics.verifyCredentials = async function (email, password) {
+    if (!email && !password)
+        return { status: 'failed', message: "Email and password required." }
+
+    if (!email)
+        return { status: 'failed', message: "Email required." }
+
+    if (!password)
+        return { status: 'failed', message: "Password required." }
+
+    let user = await this.findOne({ email: email });
+
+    if (!user)
+        return { status: 'failed', message: "No account found." }
+
+    let valid = await bcrypt.compare(password, user.password);
+    if (!valid)
+        return { status: 'failed', message: "Incorrect password." }
+
+    return {
+        status: 'success', user: user, message: "Verification successful."
+    }
+}
+
+userModel.methods.createAccessToken = async function () {
+    let accessToken = await jwt.sign(
+        { user_id: this._id },
+        process.env.SECRET,
+        { algorithm: 'HS256', expiresIn: '1d' });
+
+    return accessToken;
+}
+
+userModel.methods.verifyAccessToken = async function (accessToken) {
+    let decoded = await jwt.verify(accessToken, process.env.SECRET);
+    let user = await this.findOne(mongoose.Types.ObjectId(decoded.user_id));
+    if (!user) return { message: 'Invalid session token' };
+    return { user: user, message: 'Success' };
+}
 
 module.exports = mongoose.model('User', userModel);
