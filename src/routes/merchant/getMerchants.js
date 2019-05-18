5d2d518fd389773c02e09b44f939478c6a2d91a4
squishy123 <christian.gnaw@gmail.com>
2019-05-18 16:38:41 -0400

    Added get merchants route

---
+++ src/routes/merchant/getMerchants.js
@@ -0,0 +1,18 @@
+const Merchant = require('../../models/merchant');
+
+import { sendPayload } from '../../middle/generalFunctions';
+
+module.exports = {
+    path: "/merchants",
+    method: "GET",
+    handler: [async (req, res, next) => {
+
+        req.payload = {
+            message: "Successfully queried merchants.",
+            status: "success", data: { merchants: await Merchant.find().select('-email').select('-password') }
+
+        };
+
+        if (next) next();
+    }, sendPayload]
+}
