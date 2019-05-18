5c15a24509754c5468add593946fa080d0173e88
squishy123 <christian.gnaw@gmail.com>
2019-05-18 04:04:40 -0400

    Added bcrypt and fixed user reg password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const userModel = new mongoose.Schema({
     first_name: { type: String, required: true },
