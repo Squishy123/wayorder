4e2dffd95a63f397ebb5492e30e35699631bbb48
squishy123 <christian.gnaw@gmail.com>
2019-05-18 03:31:45 -0400

    Added merchantFunctions and merchant middle

--- src/middle/merchantFunctions.js
+++ src/middle/merchantFunctions.js
@@ -1,9 +1,72 @@
-function validateMerchant(req, res, next) {
+import { sendPayload } from './generalFunctions';
 
-    if(next)
+function validateMerchant(req, res, next) {
+    req.payload.message = [];
+
+    let fnLength = req.params.first_name.length;
+    if (fnLength < 2) {
+        req.payload.status = 'failed';
+        req.payload.message.push('First name length is less than 2');
+    }
+
+    let regex = /\S+@\S+\.\S+/;
+    if (!regex.test(req.params.email)) {
+        req.payload.status = 'failed';
+        req.payload.message.push('Invalid email address');
+    }
+
+    regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    if (!regex.test(req.params.password)) {
+        req.payload.status = 'failed';
+        req.payload.message.push('Invalid password');
+    }
+
+    if (req.payload.status === 'failed')
+        return sendPayload;
+
+    if (next)
         next();
 }
 
+function createMerchant(req, res, next) {
+    let merchant = new Merchant();
+    merchant.name = req.params.name;
+    merchant.email = req.params.email;
+    merchant.password = req.params.password;
+    await merchant.save();
+
+    req.scope.merchant = merchant;
+
+    if (next)
+        next();
+}
+
+async function checkIfMerchantExistsNotVerified(req, res, next) {
+    if (req.params.name && req.params.email) {
+        if (await User.findOne({ email: req.params.email })) {
+            req.payload.status = 'failed';
+            req.payload.message = 'Merchant already exists';
+            return sendPayload;
+        };
+    }
+
+    if(next) next();
+}
+
+function emailVerified(req, res, next) {
+    let info = await this.binds.transporter.sendMail({
+        from: "'WayOrder'<service@wayorder.com>",
+        to: `${req.params.email}`,
+        subject: "Welcome to WayOrder! Please confirm your account",
+        text: `Please visit the following link in order to confirm your account registration: wayorder.com/confirm?confirmation_token=${req.scope.confirmation_token}`,
+    })
+
+    if(next) next();
+}
+
 module.exports = {
-    validateMerchant: validateMerchant
+    validateMerchant: validateMerchant,
+    createMerchant: createMerchant,
+    checkIfMerchantExistsNotVerified: checkIfMerchantExistsNotVerified,
+    emailVerified: emailVerified
 }
--- src/models/merchant.js
+++ src/models/merchant.js
@@ -6,6 +6,63 @@ const merchantModel = new mongoose.Schema({
     password: { type: String, required: true },
     branches: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Store' }],
     meta: { Type: Object },
+    email_confirm: { type: String },
+    is_verified: { type: Boolean, default: false }
 });
+
+merchantModel.methods.addHashedPassword = async function(password) {
+    let hashed = await new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) reject(err);
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) reject(err);
+                resolve(hash);
+            });
+        });
+    });
+    this.password = hashed;
+    await this.save();
+    return hashed;
+};
+
+merchantModel.methods.createEmailConfirmationToken = async function() {
+    let confirmationToken = await jwt.sign(
+        { merchant_id: this._id },
+        process.env.SECRET,
+        { algorithm: 'HS256', expiresIn: '1d' }
+    );
+    this.email_confirm = confirmationToken;
+    await this.save();
+    return confirmationToken;
+};
+
+merchantModel.statics.verifyEmailConfirmationToken = async function(
+    confirmationToken
+) {
+    let decoded = await jwt.verify(confirmationToken, process.env.SECRET);
+    let merchant = await this.findOne(mongoose.Types.ObjectId(decoded.merchant_id));
+
+    if (confirmationToken == merchant.email_confirm)
+        return {
+            verified: true,
+            merchant: merchant,
+            status: 'success',
+            message: 'Successfully confirmed.',
+        };
+
+    if (merchant.is_verified == true)
+        return {
+            verified: false,
+            status: 'fail',
+            message: 'User already registered.',
+        };
+
+    return {
+        verified: false,
+        status: 'fail',
+        message: 'Invalid confirmation token.',
+    };
+};
+
 
 module.exports = mongoose.model('Merchant', merchantModel);
