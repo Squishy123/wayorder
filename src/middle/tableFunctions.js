0aacca9496c07af06bef1a4945627d2fbe8f0460
squishy123 <christian.gnaw@gmail.com>
2019-05-18 12:34:01 -0400

    Fixed up errs

--- src/middle/tableFunctions.js
+++ src/middle/tableFunctions.js
@@ -4,15 +4,17 @@ const Table = require('../models/table');
 async function addTable(req, res, next) {
     let table = new Table();
     table.space = req.params.space;
-    await tabale.save();
-
-    req.scope.merchant.tables.push(product);
+    await table.save();
+    
+    req.scope.merchant.capacity += req.params.space;
+    req.scope.merchant.tables.push(table);
+    req.scope.merchant.open_tables.push(table);
     await req.scope.merchant.save();
 
     req.payload = {
         message: 'Successfully added table.',
         status: 'success',
-        data: { product: product },
+        data: { table: table},
     };
 
     if (next) next();
