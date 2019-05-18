d3000ad9d0f576877b20616234f2049fd51e70c5
squishy123 <christian.gnaw@gmail.com>
2019-05-18 12:10:45 -0400

    Added default for table availablity

--- src/models/table.js
+++ src/models/table.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const tableModel = new mongoose.Schema({
     space: { type: Number, required: true },
-    status: { type: String, enum: ['available', 'unavailable'] },
+    status: { type: String, enum: ['available', 'unavailable'] , default: 'available'},
 });
 
 module.exports = mongoose.model('Table', tableModel);
