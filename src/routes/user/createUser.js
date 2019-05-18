f8a7f3cb81a7a7de1129a2ef1363e26ac379c3a5
squishy123 <christian.gnaw@gmail.com>
2019-05-18 04:00:11 -0400

    Added working registration and email confirmation outbox

---
+++ src/routes/user/createUser.js
@@ -0,0 +1,13 @@
+import * as user from '../../middle/userFunctions';
+import * as general from '../../middle/generalFunctions';
+
+module.exports = {
+    path: '/user',
+    method: 'POST',
+    handler: [
+        user.validateUserCredentials,
+        user.checkIfUserExistsNotVerified,
+        user.createUser,
+        user.emailVerified,
+        general.sendPayload]
+}
